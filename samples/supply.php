0a26a34b436113b336e9d7f3ffbd9acdef0a45df
KYO <yong.kuang@rightic.cn>
2019-06-24 13:29:07 +0800

    'tag'

--- samples/supply.php
+++ samples/supply.php
@@ -7,8 +7,8 @@
  */
 require_once __DIR__ . '/common.php';
 
-$app_id    = '2916ce5c8c31bbcf9d34954e0a98cbb3';
-$app_key   = 'W0ZkINUqP9gm0D0n';
+$app_id    = '********************';
+$app_key   = '********************';
 $sign_type = "MD5";
 
 $client = new \ICHUB\IchubClient($app_id, $app_key, $sign_type);
